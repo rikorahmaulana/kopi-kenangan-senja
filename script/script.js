49cd79ae14565bbd5cbe25527e47b07e63ad9085
riko <rahmaulanarikko@gmail.com>
2023-07-07 22:05:11 +0700

    finish navbar

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// klik di luar sidebar untuk menghilangkan nav
+// klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
 document.addEventListener("click", function (e) {
